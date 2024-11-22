2f587acd4eae3d21de06723b2257a6c47b678b91
Danilo Cuculić <danilo.cuculic@gmail.com>
2024-11-22 18:10:21 +0100

    Release 3.9.0: upgraded UIkit framework to 3.21.16, added App.CSS/JS example

---
+++ assets/es6/app.js
@@ -0,0 +1,10 @@
+//=DISABLE js/gmaps.utils.js
+
+//=DISABLE system/assets/js/framework.js
+//=DISABLE system/assets/js/framework.extras.js
+//=require js/framework.js
+//=require js/framework.extras.js
+
+//=DISABLE castus/curator/assets/js/ajaxinit.js
+
+//=DISABLE uikit/dist/js/uikit-icons.js
